6832f2178f9eb258936c64fd567159d499d3869a
Brandon Weygant <bobbydigital108@gmail.com>
2019-05-10 15:04:26 -0400

    Fixed editing function, and improved specific erb views.

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -43,49 +43,37 @@ class StoryController < ApplicationController
   end
 
   get '/stories/:id/edit' do
-    if logged_in?
-      @story = Story.find_by_id(params[:id])
-      if @story && @story.user == current_user
+    @story = Story.find_by_id(params[:id])
+      if logged_in? && @story.user_id == @current_user.id
         erb :'stories/edit_story'
       else
-        redirect to '/stories'
+        erb :'stories/edit_story'
       end
-    else
-      redirect to '/login'
-    end
   end
 
   patch '/stories/:id' do
-    if logged_in?
-      if params[:title] == "" || params[:genre] == "" || params[:synopsis] == ""
-        redirect to "/stories/#{params[:id]}/edit"
-      else
-      @story = Story.find_by_id(params[:id])
-        if @story && @story.user == current_user
-          if @story.update(title: params[:title])
-            redirect to "/stories/#{@story.id}"
-          else
-            redirect to "/stories/#{@story.id}/edit"
-          end
-        else
-          redirect to '/stories'
-        end
-      end
-    else
-      redirect to '/login'
-    end
+    @story = Story.find_by(id: params[:id])
+    @story.update(params[:story])
+
+    redirect to "/stories/#{@story.id}"
   end
 
-  delete '/stories/:id/delete' do
-    if logged_in?
-      @story = Story.find_by_id(params[:id])
-      if @story && @story.user == current_user
-        @story.delete
-      end
-      redirect to '/stories'
-    else
-      redirect to '/login'
-    end
+  post '/stories/:id' do
+    @story = Story.find_by_id(params[:id])
+    @story.title = params[:title]
+    @story.genre = params[:genre]
+    @story.synopsis = params[:synopsis]
+    @story.save
+    redirect to "/stories/#{@story.id}"
   end
+
+  delete '/stories/:id'do
+    @story = Story.find_by_id(params[:id])
+
+      if logged_in? && @story.user_id == current_user.id
+        @story.destroy
+      end
+      erb :'stories/stories'
+    end
 
 end
